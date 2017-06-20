6e63907a9ca2370fc8711a74f369329780d36fc7
TrevorJamesH <jetsamrover@gmail.com>
2017-06-19 18:03:28 -0700

    adds some routing and db posts

--- public/script.js
+++ public/script.js
@@ -1,1 +1,15 @@
 console.log('hello from the browser JavaScript')
+
+function login(){
+  console.log('clicked login')
+
+}
+
+function signup(){
+  console.log('clicked signup')
+  window.location = '/signup'
+}
+
+function getUsername(){
+  return 'Test'
+}
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,29 @@
 const express = require('express')
 const router = express.Router()
+const {signup, getAllAlbums} = require('../db/db')
 
 module.exports = function(app) {
+
   router.get('/', (req, res) => {
     res.render('landing')
+  })
+
+  router.get('/signup', (req, res) => {
+    res.render('signup')
+  })
+
+  router.post('/signup', (req, res) => {
+    console.log('signup req.body',req.body)
+    signup(req.body.name, req.body.password)
+    .then( response => {
+      console.log('signup res', response)
+      res.cookie('user_id', response[0].id, { maxAge: (100000), httpOnly: false })
+      res.redirect('/albums')
+    })
+  })
+
+  router.get('/albums', (req, res) => {
+    res.render('albums')
   })
 
   return router
